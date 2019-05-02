9a2654dc1b27b80b1cb38567c9a1e90a7588a496
Ahmed Sellou <35193952+Sellou5@users.noreply.github.com>
2019-05-02 14:28:19 +0200

    Add files via upload

---
+++ ShoppingSite/DBLayer/DBLayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ShoppingSite
+{
+    public class DBLayer
+    {
+        public static DataTable Select(SqlCommand cmd)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["shopcon"].ConnectionString);
+            cmd.Connection = con;
+
+            SqlDataAdapter adapt = new SqlDataAdapter();
+            adapt.SelectCommand = cmd;
+
+            DataTable dt = new DataTable();
+            adapt.Fill(dt);
+
+            return dt;
+        }
+
+        public static int DML(SqlCommand cmd)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["shopcon"].ConnectionString);
+            cmd.Connection = con;
+
+            con.Open();
+
+            int EffectedRows = cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            return EffectedRows;
+
+        }
+
+    }
+}
